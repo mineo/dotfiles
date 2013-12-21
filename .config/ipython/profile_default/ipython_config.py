89d3223bed820b47e32a1be158a9e707fd33401c
Wieland Hoffmann <themineo@gmail.com>
2013-12-21 01:14:57 +0100

    add an ipython config file to quit on ^D

---
+++ .config/ipython/profile_default/ipython_config.py
@@ -0,0 +1,5 @@
+c = get_config()
+# Set to confirm when you try to exit IPython with an EOF (Control-D in Unix,
+# Control-Z/Enter in Windows). By typing 'exit' or 'quit', you can force a
+# direct exit without any confirmation.
+c.InteractiveShell.confirm_exit = False

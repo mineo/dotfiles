28c4421de5419e4b636830a3c0b6d143b7407668
Wieland Hoffmann <themineo@gmail.com>
2014-12-01 19:12:18 +0100

    ipython: default to true division

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -3,3 +3,4 @@ c = get_config()
 # Control-Z/Enter in Windows). By typing 'exit' or 'quit', you can force a
 # direct exit without any confirmation.
 c.InteractiveShell.confirm_exit = False
+c.InteractiveShellApp.exec_lines = ["from __future__ import division"]

afb500fe649d1cd4c09f5e345f033e9107350546
Wieland Hoffmann <themineo@gmail.com>
2015-01-18 22:20:54 +0100

    ibuffer

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -109,6 +109,8 @@
     )
   )
 
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
 (require 'use-package)
 
 (use-package tex-site
@@ -336,6 +338,27 @@
         highlight-symbol-idle-delay 0.4
   )
 )
+
+(use-package ibuffer-vc
+  :ensure
+  :config
+  (add-hook 'ibuffer-hook
+            (lambda ()
+              (ibuffer-vc-set-filter-groups-by-vc-root)
+              (unless (eq ibuffer-sorting-mode 'alphabetic)
+                (ibuffer-do-sort-by-alphabetic))))
+  (setq ibuffer-formats
+        '((mark modified read-only vc-status-mini " "
+                (name 18 18 :left :elide)
+                " "
+                (size 9 -1 :right)
+                " "
+                (mode 16 16 :left :elide)
+                " "
+                (vc-status 16 16 :left)
+                " "
+                filename-and-process)))
+  )
 
 (use-package ignoramus
   :ensure

99e82c9b3efb9bee695de32fa74c89f3a114c650
Wieland Hoffmann <themineo@gmail.com>
2015-02-21 11:23:38 +0100

    auto-insert

--- .emacs.d/lisp/templates.el
+++ .emacs.d/lisp/templates.el
@@ -1,8 +1,36 @@
-;;; templates --- Summary
+;;; templates.el --- File templates with yasnippet   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Wieland Hoffmann
+
+;; Author: Wieland Hoffmann
+;; Keywords: files, convenience
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
+
+;;
+
 ;;; Code:
 
 (require 'yasnippet)
+
+(provide 'templates)
+;;; templates.el ends here
+
 
 (defgroup mineo-template
   nil

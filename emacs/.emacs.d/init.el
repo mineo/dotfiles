869db792e8833dafd220d20f3356d2e04aef59ad
Wieland Hoffmann <themineo@gmail.com>
2015-03-10 13:32:55 +0100

    add evil-args

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -192,6 +192,22 @@
   (define-key evil-visual-state-map (kbd ";") 'helm-M-x)
   (evil-mode))
 
+(use-package evil-args
+  :ensure
+  :config
+  ;; bind evil-args text objects
+  (define-key evil-inner-text-objects-map "a" 'evil-inner-arg)
+  (define-key evil-outer-text-objects-map "a" 'evil-outer-arg)
+
+  ;; bind evil-forward/backward-args
+  (define-key evil-normal-state-map "L" 'evil-forward-arg)
+  (define-key evil-normal-state-map "H" 'evil-backward-arg)
+  (define-key evil-motion-state-map "L" 'evil-forward-arg)
+  (define-key evil-motion-state-map "H" 'evil-backward-arg)
+
+  ;; bind evil-jump-out-args
+  (define-key evil-normal-state-map "K" 'evil-jump-out-args))
+
 (use-package ace-jump-mode
   :ensure
   :config

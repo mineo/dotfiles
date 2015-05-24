47686a630fc7178ed14f9664c5691d5b2d42b72f
Wieland Hoffmann <themineo@gmail.com>
2015-05-24 22:20:33 +0200

    remove marmalade

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -40,7 +40,6 @@
 
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
 
 (setq-default line-spacing 2)

42095b99accb493d3133e2e19d5fd07f0b042eab
Wieland Hoffmann <themineo@gmail.com>
2016-04-17 12:33:58 +0200

    Exclude evil-jumper
    because I don't like it warning me about being obsolete *all the time*

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -53,7 +53,7 @@ values."
    ;; configuration in `dotspacemacs/config'.
    dotspacemacs-additional-packages '(editorconfig github-clone pkgbuild-mode)
    ;; A list of packages and/or extensions that will not be install and loaded.
-   dotspacemacs-excluded-packages '(pyenv-mode)
+   dotspacemacs-excluded-packages '(pyenv-mode evil-jumper)
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
    ;; are declared in a layer which is not a member of
    ;; the list `dotspacemacs-configuration-layers'. (default t)

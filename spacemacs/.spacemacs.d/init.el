cb9cfe6e3053a2c58ce95c1197232942da05858f
Wieland Hoffmann <themineo@gmail.com>
2016-07-27 21:04:49 +0200

    Add a yaml layer
    I sometimes need it, e.g. for .travis.yml

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -46,6 +46,7 @@ values."
       :variables syntax-checking-enable-tooltips nil)
      theming
      version-control
+     yaml
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these

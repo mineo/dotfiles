c5a2d98576a7dc781a75cc13bc15af0dae6bae35
Wieland Hoffmann <themineo@gmail.com>
2016-10-27 22:09:22 +0200

    Fix the last commit
    What I committed was the 2-second xscreensaver demo call, not the
    deactivate one :/

--- mpv/.config/mpv/scripts/disable-xscreensaver.lua
+++ mpv/.config/mpv/scripts/disable-xscreensaver.lua
@@ -1,9 +1,9 @@
 local utils = require 'mp.utils'
 local msg = require 'mp.msg'
 
-timer = mp.add_periodic_timer(2, function()
+timer = mp.add_periodic_timer(600, function()
                         msg.verbose("Calling xscreensaver-command -deactivate")
-                        proc_table = {["args"] = {"xscreensaver-command", "-demo"}}
+                        proc_table = {["args"] = {"xscreensaver-command", "-deactivate"}}
                         proc = utils.subprocess(proc_table)
                         if proc.error ~= nil then
                           msg.info("Couldn't call xscreensaver")

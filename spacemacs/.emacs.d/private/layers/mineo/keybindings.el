c428a32799eeaa5f2a553fb119ce0b6a9843c02b
Wieland Hoffmann <themineo@gmail.com>
2020-06-01 10:44:12 +0200

    Unbind hippie-expand from TAB
    It breaks inserting tabs in makefiles.

--- spacemacs/.emacs.d/private/layers/mineo/keybindings.el
+++ spacemacs/.emacs.d/private/layers/mineo/keybindings.el
@@ -36,7 +36,6 @@
 (spacemacs/declare-prefix "o o" "org")
 
 (define-key evil-insert-state-map "\C-a" 'company-complete)
-(define-key evil-insert-state-map (kbd "TAB") 'hippie-expand)
 
 (provide 'keybindings)
 ;;; keybindings.el ends here
